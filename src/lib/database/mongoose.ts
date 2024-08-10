d9b1d79cadfbca052e36e4c63129e6e832ffa344
kainat224 <anmolkainat244@gmail.com>
2024-08-10 07:07:33 -0700

    change the databbase name

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
     cached.promise = 
         cached.promise || 
         mongoose.connect(MONGODB_URL, { 
-            dbName: 'MagicLand', bufferCommands: false 
+            dbName: 'maggic-land', bufferCommands: false 
         })
 
     cached.conn = await cached.promise;
